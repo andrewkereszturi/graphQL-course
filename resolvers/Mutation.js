3b79128ae46a953e916dc5a2d5418b041b92f5c0
andrewkereszturi <andrew.kereszturi@gmail.com>
2022-04-20 14:50:41 -0700

    GraphQL project initial commit

---
+++ resolvers/Mutation.js
@@ -0,0 +1,76 @@
+const { v4: uuid } = require("uuid");
+const { categories } = require("../db");
+
+exports.Mutation = {
+    addCategory: (parent, { input }, { db }) => {
+
+        const { name } = input;
+
+        const newCategory = {
+            id: uuid(),
+            name
+        }
+
+        db.categories.push(newCategory);
+
+        return newCategory;
+    },
+    addProduct: (parent, { input }, { db }) => {
+        const {
+            name,
+            image,
+            price,
+            onSale,
+            quantity,
+            categoryId
+        } = input
+
+        const newProduct = {
+            id: uuid(),
+            name,
+            image,
+            price,
+            onSale,
+            quantity,
+            categoryId
+        }
+
+        db.products.push(newProduct);
+
+        return newProduct;
+    },
+    addReview: (parent, { input }, { db }) => {
+        const {
+            date,
+            title,
+            comment,
+            rating,
+            productId
+        } = input
+
+        const newReview = {
+            id: uuid(),
+            date,
+            title,
+            comment,
+            rating,
+            productId
+        }
+
+        db.reviews.push(newReview);
+
+        return newReview;
+    },
+    deleteCategory: (parent, { id }, { db }) => {
+        db.categories = db.categories.filter((category) => category.id !== id);
+        db.products = db.producs.map((product) => {
+            if (product.categoryId === id)
+             return {
+                ...product,
+                categoryId: null
+            }
+            else return product;
+        });
+        return true;
+    }
+}
